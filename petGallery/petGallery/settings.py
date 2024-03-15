7c4c62d8c2695c69c66a02ea5c209bcba871d9cf
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2024-03-15 11:26:24 +0100

    Create User Model

--- petGallery/petGallery/settings.py
+++ petGallery/petGallery/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.MultiPartParser",
+    ],
+}
+
+AUTH_USER_MODEL = "users.CustomUser"
--- petGallery/users/models.py
+++ petGallery/users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+GENDER_CHOICES = (("MALE", "Male"), ("FEMALE", "Female"), ("OTHER", "Other"))
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    name = models.CharField(_("Name"), max_length=150, unique=True)
+    pet_code = models.CharField(_("Pet Code"), max_length=10, unique=True)
+    age = models.IntegerField(_("Age"))
+    gender = models.CharField(_("Gender"), max_length=20, choices=GENDER_CHOICES)
+    animal = models.CharField(_("Animal"), max_length=50)
+    breed = models.CharField(_("Breed"), max_length=50)
+
+    # Define the field to use as the unique identifier for the user
+    USERNAME_FIELD = "pet_code"
+
+    # Define the required fields for creating a user
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.name
