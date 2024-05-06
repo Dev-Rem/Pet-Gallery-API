5fa6a7d86c1bdd36dd8b695bfea0c4a8bfecd25a
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2024-05-06 16:51:57 +0100

    Add comments for swagger and complicated logics

--- petGallery/posts/views/views1.py
+++ petGallery/posts/views/views1.py
@@ -21,6 +21,9 @@ class PostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated, IsOwner]
 
     def get(self, request, *args, **kwargs):
+        """
+        This route is for getting the posts that the pet(user) created
+        """
         try:
             posts = Post.objects.filter(
                 user=request.user, is_archived=False, is_deleted=False
@@ -34,13 +37,23 @@ class PostView(generics.GenericAPIView):
             )
 
     def delete(self, request, *args, **kwargs):
+        """
+        This route is for deleting a post the pet(user) created
+        {
+            "id": post_id
+        }
+        """
         try:
+            # get post obj and check if the user owns the post obj
             post = Post.objects.get(id=request.data["id"])
             if post.user == request.user:
+
+                # check if post is archived and and delete the ArchivePost relationship obj
                 if post.is_archived:
                     archive_obj = ArchivePost.objects.get(user=request.user, post=post)
                     archive_obj.delete()
                     post.is_archived = False
+                # set is_deleted flag to True, save post and return Response
                 post.is_deleted = True
                 post.save()
                 return Response(
@@ -52,7 +65,6 @@ class PostView(generics.GenericAPIView):
                     {"message": "You can not perform this action"},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
-
         except:
             return Response(
                 {"message": "Something went wrong please try again"},
@@ -60,32 +72,40 @@ class PostView(generics.GenericAPIView):
             )
 
     def put(self, request, *args, **kwargs):
+        """
+        This route is for editing a post the pet(user) created
+        {
+            "id": post_id
+        }
+        """
         try:
+            # get post obj and check is the user owns the post
             post = Post.objects.get(id=request.data["id"])
+            if request.user == post.user:
+                # replace old values with new ones
+                post.caption = request.data["caption"]
+                post.location = request.data["location"]
 
-            post.caption = request.data["caption"]
-            post.location = request.data["location"]
+                # handle user tags
+                new_tagged_users = CustomUser.objects.filter(
+                    username__in=request.data["tags"]
+                )
+                post.tags.set(new_tagged_users)
+                # handle hashtags
+                if request.data["hashtags"] == []:
+                    post.hashtags.clear()
+                else:
+                    post.hashtags.clear()
+                    post_hashtags = [
+                        Hashtag.objects.get_or_create(name=hashtag)[0].id
+                        for hashtag in request.data["hashtags"]
+                    ]
+                    post.hashtags.add(*post_hashtags)
+                post.save()
 
-            new_tagged_users = CustomUser.objects.filter(
-                username__in=request.data["tags"]
-            )
-            post.tags.set(new_tagged_users)
-
-            if request.data["hashtags"] == []:
-                post.hashtags.clear()
-            else:
-                post.hashtags.clear()
-                post_hashtags = [
-                    Hashtag.objects.get_or_create(name=hashtag)[0].id
-                    for hashtag in request.data["hashtags"]
-                ]
-                post.hashtags.add(*post_hashtags)
-
-            post.save()
-            # serializer data and return Response
-            serializer = PostSerializer(post)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+                # serializer data and return Response
+                serializer = PostSerializer(post)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
         except:
             return Response(
                 {"message": "Something went wrong pelase try again"},
@@ -93,6 +113,15 @@ class PostView(generics.GenericAPIView):
             )
 
     def post(self, request, *args, **kwargs):
+        """
+        This route is for the pet(user) to create a post, it accepts only form data becuase of the image handling.
+        form fields include:
+        caption - "caption for the post"
+        location -  "location of the post"
+        tags - "username,username,username" - a string of username of the tagged users seperated by commas
+        hashtags - "name,name,name" - a string of hashtag names (new or old) seperated by commas
+        images - a list of images
+        """
         try:
             # create a post object
             post = Post.objects.create(
@@ -147,19 +176,25 @@ class ArchivePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated, IsOwner]
 
     def get(self, request, *args, **kwargs):
+        """
+        This route is for getting posts archived by the pet(user)
+        """
         archived_posts = ArchivePost.objects.filter(user=request.user)
         serializer = ArchivePostSerializer(archived_posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
         """
-        This view allows user to add a post from archive
+        This view allows pet(user) to add a post to archive, the pet(user) can only archive their own post.
+        {
+            "id": post_id
+        }
         """
         try:
-            # get the post
-            post = Post.objects.get(id=request.data["post"])
-
+            # get the post obj and check if the requesting user is the owner of the post
+            post = Post.objects.get(id=request.data["id"])
             if post.user == request.user:
+                # create archive obj and set post is_archived flag to True, and save.
                 archive_obj = ArchivePost.objects.create(user=request.user, post=post)
                 post.is_archived = True
                 post.save()
@@ -178,16 +213,22 @@ class ArchivePostView(generics.GenericAPIView):
 
     def put(self, request, *args, **kwargs):
         """
-        This view allows user to remove a post from archive
+        This route is for the pet(user) to remove a post from archive.
+        {
+            "id": archive_obj_id
+        }
+        archive_obj_id: The id of the ArchivePost obj to be removed
         """
         try:
             # get the archived post
             archived_post = ArchivePost.objects.get(id=request.data["id"])
-
+            # check if the requesting user is the owner of the archived post and get the main post obj
             if archived_post.user == request.user:
                 post = Post.objects.get(id=archived_post.post.id)
+                # set is_archived flag of the main post obj to False and save
                 post.is_archived = False
                 post.save()
+                # delete the archived post relationship obj
                 archived_post.delete()
                 return Response(
                     {"message": "Post removed from archive successfully"},
@@ -211,7 +252,14 @@ class SavePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated, IsOwner]
 
     def post(self, request, *args, **kwargs):
+        """
+        This route is for a pet(user) to save any post
+        {
+            "id": post_id
+        }
+        """
         try:
+            # try to get or create the pet(user) SavePost obj, add the post to their saved posts
             user_save_post_obj, created = SavePost.objects.get_or_create(
                 user=request.user
             )
@@ -235,8 +283,13 @@ class SavePostView(generics.GenericAPIView):
             )
 
     def get(self, request, *args, **kwargs):
+        """
+        This route is for getting all the posts saved by the pet(user)
+        """
         try:
-            saved_posts_objs = SavePost.objects.get(user=request.user)
+            saved_posts_objs, created = SavePost.objects.get_or_create(
+                user=request.user
+            )
             serializer = SavePostSerializer(saved_posts_objs)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except ValidationError as e:
@@ -249,8 +302,16 @@ class SavePostView(generics.GenericAPIView):
             )
 
     def put(self, request, *args, **kwargs):
+        """
+        This route is for a pet(user) to remove a post from saved posts
+        {
+            "id": post_id
+        }
+        """
         try:
-            saved_posts_objs = SavePost.objects.get(user=request.user)
+            saved_posts_objs, created = SavePost.objects.get_or_create(
+                user=request.user
+            )
             saved_posts_objs.posts.remove(request.data["id"])
             serializer = SavePostSerializer(saved_posts_objs)
             return Response(serializer.data, status=status.HTTP_200_OK)
--- petGallery/posts/views/views2.py
+++ petGallery/posts/views/views2.py
@@ -9,7 +9,6 @@ from utils.permissions import IsOwner
 
 
 # things to be don
-# Views to handle comment replies
 # how to get post and comments in one response object
 
 
@@ -19,13 +18,17 @@ class FeedPostsView(generics.ListAPIView):
     permission_classes = [IsAuthenticated, IsOwner]
 
     def get(self, request, *args, **kwargs):
+        """
+        This route is for getting posts for the pet(user) feed, this are posts from accounts the pet(user) follows.
+        """
         try:
+            # get the requesting users account obj and get the usernames of the account the user follows
             user_account = Account.objects.get(user=request.user)
             following_usernames = [
                 following.following.user.username
                 for following in user_account.following.all()
             ]
-
+            # get posts based on following_usernames, serialize and return Response
             posts = Post.objects.filter(user__username__in=following_usernames)
             serializer = PostSerializer(posts, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -45,6 +48,9 @@ class ExplorePostsView(generics.ListAPIView):
     permission_classes = [IsAuthenticated, IsOwner]
 
     def get(self, request, *args, **kwargs):
+        """
+        This route is for getting posts for the explore feed
+        """
         try:
             users_accounts = Account.objects.all()
             accounts_usernames = [account.user.username for account in users_accounts]
@@ -67,6 +73,9 @@ class TaggedPostsView(generics.RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated, IsOwner]
 
     def get(self, request, *args, **kwargs):
+        """
+        This route is for getting all the post the pet(user) has been tagged in
+        """
         try:
             posts = Post.objects.filter(tags__username=request.user)
             serializer = PostSerializer(posts, many=True)
@@ -81,9 +90,17 @@ class TaggedPostsView(generics.RetrieveUpdateAPIView):
             )
 
     def put(self, request, *args, **kwargs):
+        """
+        This route is for the user to remove themself from the tag list of a post
+        {
+            "id": post_id
+        }
+        """
         try:
+            # get the post obj and check if the user is in the tag list
             post = Post.objects.get(id=request.data["id"])
             if request.user in post.tags.all():
+                # remove user from tag list, save post obj, and return Response
                 post.tags.remove(request.user)
                 post.save()
                 return Response(
@@ -113,6 +130,9 @@ class LikePostsView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated, IsOwner]
 
     def get(self, request, *args, **kwargs):
+        """
+        This route is for getting posts that the pet(user has liked)
+        """
         try:
             posts = Post.objects.filter(likes__username=request.user)
             serializer = PostSerializer(posts, many=True)
@@ -127,6 +147,12 @@ class LikePostsView(generics.ListCreateAPIView):
             )
 
     def post(self, request, *args, **kwargs):
+        """
+        This route is for liking and unliking a post
+        {
+            "id": post_id
+        }
+        """
         try:
             post = Post.objects.get(id=request.data["id"])
             if request.user not in post.likes.all():
@@ -177,8 +203,14 @@ class CommentsView(generics.GenericAPIView):
         return comments_with_replies
 
     def get(self, request, *args, **kwargs):
+        """
+        This route is for getting all the comments fo a post and its replies
+        {
+            "id": post_id
+        }
+        """
         try:
-            post = Post.objects.get(id=request.data["post"])
+            post = Post.objects.get(id=request.data["id"])
             comments_with_replies = self.get_comments_with_replies(post)
             return Response(comments_with_replies, status=status.HTTP_200_OK)
 
@@ -192,7 +224,19 @@ class CommentsView(generics.GenericAPIView):
             )
 
     def post(self, request, *args, **kwargs):
+        """
+        This route is for creating a comment and also replying to a comment
+        for comments
+        {
+            "post": post_id
+        }
+        for replies
+        {
+            "comment": comment_id
+        }
+        """
         try:
+            # check if post id was sent in request data if not then it is a reply to a comment
             if "post" in request.data:
                 post = Post.objects.get(id=request.data["post"])
                 comment = Comment.objects.create(
@@ -226,10 +270,18 @@ class CommentsView(generics.GenericAPIView):
             )
 
     def put(self, request, *args, **kwargs):
+        """
+        This route is for editing comments the pet(user) has created
+        {
+            "id": comment_id
+        }
+        """
         try:
+            # get the comment object and check if the pet(user) is the owner of the comment obj, then assign new text value.
             comment = Comment.objects.get(id=request.data["id"])
             if request.user == comment.user:
                 comment.text = request.data["text"]
+                # change the edited flag to True to show it was edited, save comment obj, and return Response
                 comment.edited = True
                 comment.save()
                 serializer = CommentSerializer(comment)
@@ -254,6 +306,12 @@ class CommentsView(generics.GenericAPIView):
             )
 
     def patch(self, request, *args, **kwargs):
+        """
+        This route is for a pet(user) to like and unlike a comment or reply
+        {
+            "id": comment_id
+        }
+        """
         try:
             comment = Comment.objects.get(id=request.data["id"])
             if request.user not in comment.likes.all():
@@ -282,9 +340,17 @@ class CommentsView(generics.GenericAPIView):
             )
 
     def delete(self, request, *args, **kwargs):
+        """
+        This route is for deleting a comment
+        {
+            "id": comment_id
+        }
+        """
         try:
+            # get the comment obj and check if the pet(user) is the owner of the comment or the owner of the post
             comment = Comment.objects.get(id=request.data["id"])
             if (request.user == comment.user) or (request.user == comment.post.user):
+                # set the is_deleted flag of the comment to True, save, and return Response
                 comment.is_deleted = True
                 comment.save()
                 return Response(
