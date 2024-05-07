9889b32251014c3dbbe65dd429999f7f0af654a9
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2024-05-07 16:48:04 +0100

    Register model on admin

--- petGallery/chats/admin.py
+++ petGallery/chats/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from chats.models import Chat
 
 # Register your models here.
+
+
+@admin.register(Chat)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "sender",
+        "receiver",
+        "message",
+        "is_edited",
+        "conversation_code",
+        "created_at",
+        "updated_at",
+    )
