c1b427b8c01af071dc5a0dba6e4709c50f5b5de8
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2024-05-17 16:43:39 +0100

    Set wsgi to use production settings

--- petGallery/petGallery/wsgi.py
+++ petGallery/petGallery/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petGallery.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "petGallery.settings.production")
 
 application = get_wsgi_application()
