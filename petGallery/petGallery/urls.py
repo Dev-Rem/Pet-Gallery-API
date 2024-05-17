8f358b25ee6fa82af92491323d928583edc5a2bd
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2024-05-17 16:44:46 +0100

    Add django debug tool to project

--- petGallery/petGallery/urls.py
+++ petGallery/petGallery/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from petGallery import settings
+from django.conf import settings
 from django.conf.urls.static import static
 from users import urls as user_urls
 from posts import urls as post_urls
@@ -60,6 +61,11 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        import debug_toolbar
+
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
