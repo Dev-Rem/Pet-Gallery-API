1fef51e8dfea70f637f51ee1a189b89b2e5943d3
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2024-05-17 16:40:11 +0100

    Update asgi to user ChatConsumer

--- petGallery/petGallery/asgi.py
+++ petGallery/petGallery/asgi.py
@@ -8,9 +8,52 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "petGallery.settings")
+# This allows easy placement of apps within the interior
+# rentalsystem directory.
+from django.urls import re_path
 
-application = get_asgi_application()
+from chats.consumer import ChatConsumer
+from utils.custom_middleware import TokenAuthMiddleware
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(ROOT_DIR / "rentalsystem"))
+
+# If DJANGO_SETTINGS_MODULE is unset, default to the local settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "petGallery.settings.local")
+
+# This application object is used by any ASGI server configured to use this file.
+django_application = get_asgi_application()
+
+# Apply ASGI middleware here.
+chat_application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": TokenAuthMiddleware(
+            URLRouter(
+                [
+                    re_path(r"ws/chat/(?P<user_id>\w+)/$", ChatConsumer.as_asgi()),
+                ]
+            )
+        ),
+    }
+)
+# from helloworld.asgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
+
+# Import websocket application here, so apps from django_application are loaded first
+from petGallery.websocket import websocket_application  # noqa isort:skip
+
+
+async def application(scope, receive, send):
+    if scope["type"] == "http":
+        await django_application(scope, receive, send)
+    elif scope["type"] == "websocket":
+        await websocket_application(scope, receive, send)
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
